e699ff4e1cb647c5b90f2c90497f75bc78c0e579
DONGL3XD <o_sunghun@naver.com>
2023-05-04 09:42:27 +0900

    [INIT] ldh - Initial Commit

---
+++ r6primis/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Let's go</h1>
+        <nav>
+
+        </nav>
+      </header>
+      <div id="body">
+
+      </div>
+      <footer>
+
+      </footer>
+    </>
+  );
+}
+
+export default App;
