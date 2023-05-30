728f66c995e6a83633f63f106d71003cdb03be71
DONGL3XD <o_sunghun@naver.com>
2023-05-30 16:11:06 +0900

    [Add] ldh - Header

--- r6primis/src/App.js
+++ r6primis/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 
+import logo from './img/logo.svg'
+
 function App() {
   return (
     <>
       <header>
-        <h1>Let's go</h1>
+        <img src={logo} alt="logo" id="main-logo" />
+        <div className="bar"></div>
         <nav>
-
+          <div className="nav-item">선수 소개</div>
+          <div className="nav-item">팀 소개</div>
+          <div className="nav-item">대회 소개</div>
+          <div className="nav-item">대회 일정표</div>
+          <div className="nav-item">경기 순위</div>
+          <div className="nav-item">다시보기</div>
         </nav>
+        <div className="bar"></div>
       </header>
       <div id="body">
 
